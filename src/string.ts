d401fb5a456ebf40a2d31a6f0c9b438e37832891
Andres Montenegro <amontenegrob@uni.pe>
2024-10-16 12:46:39 -0500

    Add string module and capitalizeFirstLetter function

---
+++ src/string.ts
@@ -0,0 +1,12 @@
+/**
+ * Capitalizes the first letter of the given string.
+ *
+ * @param str - The string to capitalize.
+ * @returns The string with the first letter capitalized.
+ */
+export const capitalizeFirstLetter = (str: string | null): string => {
+  if (!str) {
+    return '';
+  }
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
